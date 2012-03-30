a2cf29acbe698bcf423910c7615df18c8d7abac6
Max Malook <max@malook.de>
2012-03-30 10:53:25 +0200

    add possibility to ignore configuration errors

--- Source/FeatureSwitcher.Configuration/WithAppConfig.cs
+++ Source/FeatureSwitcher.Configuration/WithAppConfig.cs
@@ -6,23 +6,49 @@ namespace FeatureSwitcher.Behaviors
     public class WithAppConfig : IControlFeatures
     {
         private readonly Configuration.Configuration _configuration;
+        private readonly bool _ignoreConfigurationErrors;
 
-        public WithAppConfig() : this(null)
+        public WithAppConfig()
+            : this(null, false)
+        {
+        }
+
+        public WithAppConfig(bool ignoreConfigurationErrors)
+            : this(null, ignoreConfigurationErrors)
         {
         }
 
         internal WithAppConfig(Configuration.Configuration configuration)
+            : this(configuration, false)
+        {
+        }
+
+        internal WithAppConfig(Configuration.Configuration configuration, bool ignoreConfigurationErrors)
         {
             _configuration = configuration;
+            _ignoreConfigurationErrors = ignoreConfigurationErrors;
         }
 
         private Configuration.Configuration Configuration
         {
+            get { return _configuration ?? ConfigurationManagerSection; }
+        }
+
+        private Configuration.Configuration ConfigurationManagerSection
+        {
             get
             {
-                return _configuration ??
-                       ConfigurationManager.GetSection("featureSwitcher") as Configuration.Configuration ??
-                       new Configuration.Configuration();
+                Configuration.Configuration configuration = null;
+                try
+                {
+                    configuration = ConfigurationManager.GetSection("featureSwitcher") as Configuration.Configuration;
+                }
+                catch (ConfigurationErrorsException)
+                {
+                    if (!_ignoreConfigurationErrors)
+                        throw;
+                }
+                return configuration ?? new Configuration.Configuration();
             }
         }
 
