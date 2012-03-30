b8fc314ec5e02ea61021cdf43d1c2bcde94faabc
Max Malook <max@malook.de>
2012-03-30 13:17:09 +0200

    add examples

---
+++ Source/FeatureSwitcher.Examples/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using FeatureSwitcher.Behaviors;
+
+namespace FeatureSwitcher.Examples
+{
+    internal class Myth : IFeature {}
+
+    internal class BlueBackground : IFeature {}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ControlFeatures.Behavior = new WithAppConfig(true);
+
+            if (Feature<BlueBackground>.IsEnabled)
+                Console.BackgroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine("Myth feature is {0}", Feature<Myth>.IsEnabled ? "enabled" : "disabled");
+            if (Debugger.IsAttached)
+                Console.ReadLine();
+        }
+    }
+}
---
+++ Source/FeatureSwitcher.Examples/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FeatureSwitcher.Examples")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("FeatureSwitcher")]
+[assembly: AssemblyProduct("FeatureSwitcher.Examples")]
+[assembly: AssemblyCopyright("Copyright © FeatureSwitcher 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e389c380-bb0c-42b5-b30b-a6fb1055f713")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
