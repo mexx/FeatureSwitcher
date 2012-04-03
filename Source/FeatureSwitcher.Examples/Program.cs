f71506fcb8129c5849f1bc90befa2890207c11ee
Max Malook <max@malook.de>
2012-04-03 16:43:31 +0200

    add API sketch for context support

--- Source/FeatureSwitcher.Examples/Program.cs
+++ Source/FeatureSwitcher.Examples/Program.cs
@@ -10,10 +10,74 @@ namespace FeatureSwitcher.Examples
 
     internal class Named: IFeature {}
 
+    internal class BusinessBranch : IFeatureContext
+    {
+        public static readonly BusinessBranch HQ = new BusinessBranch();
+
+        private BusinessBranch()
+        {
+        }
+    }
+
+    public static class F
+    {
+        public static FeatureSettings ControlFeatures(this IFeatureContext This)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static FeatureWithContext<T> Feature<T>(this IFeatureContext This, T feature) where T:IFeature
+        {
+            return new FeatureWithContext<T>(This);
+        }
+
+        public static FeatureWithContext<T> Feature<T>(this IFeatureContext This) where T : IFeature
+        {
+            return new FeatureWithContext<T>(This);
+        }
+    }
+
+    public class FeatureSettings
+    {
+        public IControlFeatures Behavior { get; set; }
+
+        public IProvideFeatureNames Name { get; set; }
+    }
+
+    public class FeatureWithContext<T>
+    {
+        private readonly IFeatureContext _context;
+
+        public FeatureWithContext(IFeatureContext context)
+        {
+            _context = context;
+        }
+
+        public bool IsEnabled
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+
+    public interface IFeatureContext
+    {
+    }
+
+    public static class Default
+    {
+        public static IFeatureContext Context
+        {
+            get { return null; }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            BusinessBranch.HQ.ControlFeatures().Behavior = Use.SettingsFrom.AppConfig().IgnoreConfigurationErrors();
+            BusinessBranch.HQ.ControlFeatures().Name = Use.Type.FullName;
+
             ControlFeatures.Behavior = Use.SettingsFrom.AppConfig().IgnoreConfigurationErrors();
             ControlFeatures.Name = Use.Type.FullName;
 
@@ -24,12 +88,23 @@ namespace FeatureSwitcher.Examples
             if (Debugger.IsAttached)
                 Console.ReadLine();
 
+
+            var branch = BusinessBranch.HQ;
             var named = new Named();
-            var enabled = named.IsEnabled();
-            var isEnabled = Feature<Named>.IsEnabled;
+
+            var a = Feature<Named>.IsEnabled;
+            var b = Default.Context.Feature<Named>().IsEnabled;
+            var c = branch.Feature<Named>().IsEnabled;
+
+            var d = named.IsEnabled();
+            var e = Default.Context.Feature(named).IsEnabled;
+            var f = branch.Feature(named).IsEnabled;
 
             var features = new IFeature[] {new Myth(), new BlueBackground()};
-            foreach (var feature in features.Where(Feature.IsEnabled))
+            foreach (var feature in features.
+                Where(Feature.IsEnabled).
+                Where(x => Default.Context.Feature(x).IsEnabled).
+                Where(x => branch.Feature(x).IsEnabled))
             {
                 feature.IsEnabled();
             }
