8a4c12bcfa4c69f736603165560a2356d3da9d15
Max Malook <community@malook.de>
2012-06-25 14:44:39 +0200

    replace public fields with properties

--- Source/FeatureSwitcher/Configuration/AllFeatures.cs
+++ Source/FeatureSwitcher/Configuration/AllFeatures.cs
@@ -2,8 +2,14 @@ namespace FeatureSwitcher.Configuration
 {
     public sealed class AllFeatures : IProvideBehavior
     {
-        public static readonly IProvideBehavior Enabled = new AllFeatures(true);
-        public static readonly IProvideBehavior Disabled = new AllFeatures(false);
+        public static IProvideBehavior Enabled { get; private set; }
+        public static IProvideBehavior Disabled { get; private set; }
+
+        static AllFeatures()
+        {
+            Enabled = new AllFeatures(true);
+            Disabled = new AllFeatures(false);
+        }
 
         private readonly bool _enabled;
 
