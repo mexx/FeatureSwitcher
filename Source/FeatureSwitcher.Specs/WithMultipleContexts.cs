189fc0aeabe50a60b0e742bfcfd6deb5a6009de4
Max Malook <community@malook.de>
2012-06-25 15:12:17 +0200

    move syntax sugar test to separate file

--- Source/FeatureSwitcher.Specs/WithMultipleContexts.cs
+++ Source/FeatureSwitcher.Specs/WithMultipleContexts.cs
@@ -83,29 +83,6 @@ namespace FeatureSwitcher.Specs
         Behaves_like<EnabledSimpleFeatureInHeadquatersBehavior> an_enabled_feature_in_headquarters;
     }
 
-    public class Syntax_sugar : WithCleanUp
-    {
-        Because of = () => Features.Are.
-            NamedBy.TypeName().And.
-            NamedBy.TypeFullName().And.
-            ConfiguredBy.AppConfig().And.
-            ConfiguredBy.AppConfig().IgnoreConfigurationErrors().And.
-            ConfiguredBy.AppConfig().IgnoreConfigurationErrors().UsingConfigSectionGroup("test").And.
-            AlwaysDisabled().And.
-            AlwaysEnabled().And.
-            NamedBy.TypeName().
-            NamedBy.TypeFullName().
-            ConfiguredBy.AppConfig().
-            NamedBy.TypeName().
-            ConfiguredBy.AppConfig().IgnoreConfigurationErrors().
-            ConfiguredBy.AppConfig().IgnoreConfigurationErrors().UsingConfigSectionGroup("test").
-            NamedBy.TypeFullName().
-            AlwaysDisabled().
-            AlwaysEnabled().
-            HandledByDefault();
-
-        It should_not_fail = () => true.ShouldBeTrue();
-    }
     // ReSharper restore InconsistentNaming
 #pragma warning restore 169
 }
