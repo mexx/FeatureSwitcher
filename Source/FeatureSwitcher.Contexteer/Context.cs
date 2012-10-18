39ce693398526506bf64547c9c4b16ed3a6106a5
Max Malook <max@malook.de>
2012-10-18 22:29:52 +0200

    extract context application to separate method

--- Source/FeatureSwitcher.Contexteer/Context.cs
+++ Source/FeatureSwitcher.Contexteer/Context.cs
@@ -6,20 +6,26 @@ namespace FeatureSwitcher
 {
     public static class Context
     {
-        public static bool EnabledInContextOf<TFeature, TContext>(this Feature.Switch.IKnowStateOf<TFeature> This, TContext context)
+        public static bool EnabledInContextOf<TFeature, TContext>(this Feature.Switch.IKnowStateOf<TFeature> This, TContext instance)
             where TFeature : IFeature
             where TContext : IContext
         {
-            var featureType = This.GetType().GetGenericArguments().First();
-            var configuration = FeatureConfiguration.For(context);
-            return Feature.Switch.For<IFeature>(featureType).With(configuration).Enabled;
+            return This.InContextOf(instance).Enabled;
         }
 
-        public static bool DisabledInContextOf<TFeature, TContext>(this Feature.Switch.IKnowStateOf<TFeature> This, TContext context)
+        public static bool DisabledInContextOf<TFeature, TContext>(this Feature.Switch.IKnowStateOf<TFeature> This, TContext instance)
             where TFeature : IFeature
             where TContext : IContext
         {
-            return !This.EnabledInContextOf(context);
+            return This.InContextOf(instance).Disabled;
+        }
+
+        private static Feature.Switch.IKnowStateOf<TFeature> InContextOf<TFeature, TContext>(this Feature.Switch.IKnowStateOf<TFeature> This, TContext instance)
+            where TFeature : IFeature
+            where TContext : IContext
+        {
+            var type = This.GetType().GetGenericArguments().First();
+            return Feature.Switch.For<TFeature>(type).With(FeatureConfiguration.For(instance));
         }
     }
 }
