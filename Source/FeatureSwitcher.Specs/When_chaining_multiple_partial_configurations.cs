6c8101e1acc424bb4935a992683d9cac6f79302d
Robert Coltheart <robert.coltheart@gmail.com>
2018-01-24 22:00:02 +0800

    [Feature] Convert to netstandard1.0

--- Source/FeatureSwitcher.Specs/When_chaining_multiple_partial_configurations.cs
+++ Source/FeatureSwitcher.Specs/When_chaining_multiple_partial_configurations.cs
@@ -12,10 +12,19 @@ namespace FeatureSwitcher.Specs
                                   ConfiguredBy.Custom(EnableByName<Simple>.IsEnabled, EnableByName<Complex>.IsEnabled).And.
                                   NamedBy.Custom(Features.OfType<Simple>.NamedByTypeName, Features.OfType<Complex>.NamedByTypeName);
 
-        Behaves_like<Disabled<Basic>> a_disabled_basic_feature;
+        class when_using_basic
+        {
+            Behaves_like<Disabled<Basic>> a_disabled_basic_feature;
+        }
 
-        Behaves_like<Enabled<Simple>> an_enabled_simple_feature;
+        class when_using_simple
+        {
+            Behaves_like<Enabled<Simple>> an_enabled_simple_feature;
+        }
 
-        Behaves_like<Enabled<Complex>> an_enabled_complex_feature;
+        class when_using_complex
+        {
+            Behaves_like<Enabled<Complex>> an_enabled_complex_feature;
+        }
     }
 }
