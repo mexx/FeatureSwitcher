1375fdbe31a28e88c0642227596f4b31b714591d
Max Malook <max@malook.de>
2018-02-07 20:39:44 +0100

    setup AppVeyor and Travis CI

---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -eu
+
+cd "$(dirname "$0")"
+
+TARGET="Default"
+if [ "$1" != "" ]
+then
+  TARGET="$1"
+fi
+
+NUGET_EXE=Source/.nuget/NuGet.exe
+FAKE_VERSION=2.2.0
+FAKE_EXE=Source/packages/FAKE/tools/FAKE.exe
+
+FSIARGS=""
+FSIARGS2=""
+OS=${OS:-"unknown"}
+if [ "$OS" != "Windows_NT" ]
+then
+  # Can't use FSIARGS="--fsiargs -d:MONO" in zsh, so split it up
+  # (Can't use arrays since dash can't handle them)
+  FSIARGS="--fsiargs"
+  FSIARGS2="-d:MONO"
+fi
+
+run() {
+  echo $@
+  if [ "$OS" != "Windows_NT" ]
+  then
+    mono "$@"
+  else
+    "$@"
+  fi
+}
+
+run $NUGET_EXE install FAKE -OutputDirectory Source/packages -ExcludeVersion -Version $FAKE_VERSION -Verbosity detailed
+run $FAKE_EXE "target=$TARGET" $FSIARGS $FSIARGS2 build.fsx
